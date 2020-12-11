3232a967e7ec964146dd150fd943d12ff08ad1e8
AleksandraArt <an78art@gmail.com>
2020-12-11 20:25:19 +0300

    v.2

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-
-
-        boolean ticket = true;
+        
         int amount = 23000;
         int bonus = amount/ 20;
         System.out.println(bonus);
